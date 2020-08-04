807939731a21599031055fb8e3cb444e7118953e
Chase <cwilson7@trinity.edu>
2020-08-04 14:24:26 -0700

    setup

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public GameObject _player;
+
+    float runSpeed = 1f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        rb.velocity = (_player.transform.position - transform.position).normalized * runSpeed;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    int groundLayer = 8;
+    float jumpHeight = 5f, runSpeed = 3f;
+    public bool jumping = false, isGrounded = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Inputs();
+    }
+
+    void Inputs()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        Vector3 inputVector = new Vector3(inputX * runSpeed, inputY * runSpeed, 0f);
+        rb.velocity = inputVector;
+    }
+}
